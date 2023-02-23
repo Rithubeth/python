22c5ba5838c1383f057b55de7256a9f23b29f664
Rithubeth G <rithubetham@gmail.com>
2023-02-23 13:28:56 +0530

    Learnings

---
+++ sd2.py
@@ -0,0 +1,10 @@
+print("This will run")
+
+#print("this won't run.")
+#You can also use a comment to "disable" or comment out code:
+
+print("I could have code like this") #and the comment after is ignored
+
+#Anything after the # is ignored byy python.
+#A comment, this is so you can read your program later.
+
---
+++ sd3.py
@@ -0,0 +1,20 @@
+print("I will now count my chickens:")
+#text is displayed
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+#two text lines printed with no.of each calculated using maths.
+print("Now I will count the eggs:")
+
+print(3+2+1-5+4%2-1/4+6)
+print("Is it true that 3+2 < 5-7?")
+print(3+2 < 5-7)
+print("What is 3+2?", 3+2)
+print("What is 5-7?", 5-7)
+
+print("Oh, that's why it's False.")
+
+print("Lets count the cars")
+print("Black cars", 50+4-12%2)
+print("white cars", 120-60*3%2)
+print("red cars", round(55.5+12.6))
+
