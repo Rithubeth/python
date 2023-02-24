93a91f86fa2d829f83b1162907a9ee265d5f0d93
Rithubeth G <rithubetham@gmail.com>
2023-02-24 16:53:07 +0530

    python

---
+++ sd11.py
@@ -0,0 +1,19 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+print("enter your name:")
+x = input()
+print("Hello MR.",x)
+
+
+x = input('A place:')
+print(x)
+
+print("Would you like to have a cup of tea?")
+input()
